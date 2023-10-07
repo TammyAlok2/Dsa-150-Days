8e6eff9e244574d809b1186378956c4a9dc50efc
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-10-07 18:48:16 +0530

    Create SEP_28_2023-sort-array-by-parity.cpp

---
+++ POTD/Leetcode/SEP_28_2023-sort-array-by-parity.cpp
@@ -0,0 +1,18 @@
+//link:https://leetcode.com/problems/sort-array-by-parity/
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+         int i = 0, j = nums.size() - 1;
+        
+        while (i < j) {
+            while (i < j && nums[i] % 2 == 0)
+                i++;
+            while (i < j && nums[j] % 2 == 1)
+                j--;
+            
+            std::swap(nums[i], nums[j]);
+        }
+        
+        return nums;
+    }
+};
