1bb89320bf80759e95bd4bad25f7565a3be38c50
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-10-07 18:51:03 +0530

    Create SEP_25_2023-find-the-differnce.cpp

---
+++ POTD/Leetcode/SEP_25_2023-find-the-differnce.cpp
@@ -0,0 +1,11 @@
+//link:https://leetcode.com/problems/find-the-difference/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+            char c = 0;
+        for(char cs : s) c ^= cs;
+        for(char ct : t) c ^= ct;
+        return c;
+    }
+};
