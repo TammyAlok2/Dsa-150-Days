cd03b8b9fa901a9fa37da46d057d53ac580e10ab
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-11-03 17:57:41 -0700

    Create NOV_04_2023-lastMoment-ant.cpp

---
+++ POTD/Leetcode/NOV_04_2023-lastMoment-ant.cpp
@@ -0,0 +1,16 @@
+//link:https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/solution/
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int ans = 0;
+        for (int num : left) {
+            ans = max(ans, num);
+        }
+        
+        for (int num : right) {
+            ans = max(ans, n - num);
+        }
+        
+        return ans;
+    }
+};
