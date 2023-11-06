45ac05e464b1d32c27f0867f840b872a941aa88b
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-11-06 16:32:41 +0530

    5 question day 1

---
+++ LoveBabbarSupreme/Daily5/sort-colors.cpp
@@ -0,0 +1,19 @@
+     
+     
+     
+      int n= arr.size();
+        int low = 0,mid= 0, high=n-1;
+        while(mid<=high){
+if(arr[mid]==0){
+swap(arr[low],arr[mid]);
+    mid++;
+    low++;
+}
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
