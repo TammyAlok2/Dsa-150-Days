58cef8c6d577b725f5efc82d4a322454468374d0
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-11-19 18:21:33 +0530

    Create NOV_19_2023_reduction-operations-to-make-the-array-elements-equal.cpp

---
+++ POTD/Leetcode/NOV_19_2023_reduction-operations-to-make-the-array-elements-equal.cpp
@@ -0,0 +1,19 @@
+//link:https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/submissions/
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int up = 0;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                up++;
+            }
+            
+            ans += up;
+        }
+        
+        return ans;
+    }
+};
