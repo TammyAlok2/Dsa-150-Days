d98e3550ebb244ed3bab0dc1fd766e34141b7edb
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-11-29 20:17:51 +0530

    Create NOV_29_2023-euler-circuit-and_path.cpp

---
+++ POTD/GFG/NOV_29_2023-euler-circuit-and_path.cpp
@@ -0,0 +1,11 @@
+class solution :
+{
+      int e=0,od=0;
+       for(int i=0;i<V;i++)
+           if(adj[i].size()%2==0)
+               e++;
+           else od++;
+       if(e==V)return 2;
+       if(od==2)return 1;
+       return 0;
+}
