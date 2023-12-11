a723da90e8d5de94ce21e82d7d45aefc6854d9ba
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-12-11 18:50:15 +0530

    Create DEC_11_2023-element-appear-more-than-25.cpp

---
+++ POTD/Leetcode/DEC_11_2023-element-appear-more-than-25.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        unordered_map<int, int> counts;
+        for (int num : arr) {
+            counts[num]++;
+        }
+        
+        int target = arr.size() / 4;
+        for (auto& [key, value] : counts) {
+            if (value > target) {
+                return key;
+            }
+        }
+        
+        return -1;
+    }
+};
