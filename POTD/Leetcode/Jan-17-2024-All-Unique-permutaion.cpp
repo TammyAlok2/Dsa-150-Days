4488887e6065b66c802eb4a21a142eec4697b58a
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-01-17 19:51:06 +0530

    Create Jan-17-2024-All-Unique-permutaion.cpp

---
+++ POTD/Leetcode/Jan-17-2024-All-Unique-permutaion.cpp
@@ -0,0 +1,21 @@
+void solve(set<vector<int>> &sv, vector<int> arr, int ind, int n) {
+        // base case
+        if(ind == n) {
+            sv.insert(arr);
+            return ;
+        }
+        
+        for(int j = ind;j < n; j++) {
+            swap(arr[ind], arr[j]);
+            solve(sv, arr, ind+1, n);  // recursive case
+            swap(arr[ind], arr[j]);
+        }
+    }
+
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        set<vector<int>> sv;
+        solve(sv, arr, 0, n);
+
+        vector<vector<int>> ans(sv.begin(), sv.end());
+        return ans;
+    }
