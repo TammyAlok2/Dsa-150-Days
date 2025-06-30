00d13d458eccca42b3416d6532cd952b2464a599
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-06-30 10:50:19 +0530

    Time: 4 ms (7.46%) | Memory: 11.1 MB (46.41%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int>result;
+        // step1-> Store array element in the map 
+        map<int,int>count;
+        for(int num:arr1){
+            count[num]++;
+        }
+
+        // step -2 ) store array 1 element according to the arr2 
+        for(int num:arr2){
+            while(count[num]-- > 0){
+             result.push_back(num);
+            }
+            count.erase(num); // already stored ko delete kar do
+        }
+        
+        // arr2 ke bache huye elmenent ko store karna hai 
+
+        for(auto it : count){
+            int num = it.first;
+            int freq = it.second;
+            while(freq-- > 0){
+                result.push_back(num);
+            }
+        }
+
+        return result;
+    }
+};
