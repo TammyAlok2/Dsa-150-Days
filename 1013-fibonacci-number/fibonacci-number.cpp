e74a0d24c6f227b6b852290f2f0cd2beb9f3aca0
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-07-13 13:32:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.23%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
 
-int fibHelper(int n, vector<int>& memo) {
-    if (n <= 1) return n;
+int solve(int n,vector<int>& memo){
+        // basa case
+        if(n<=1){
+            return n;
+        }
 
-    if (memo[n] != -1) return memo[n];
+        if(memo[n]!=-1){
+            return memo[n];
+        }
 
-    memo[n] = fibHelper(n - 1, memo) + fibHelper(n - 2, memo);
-    return memo[n];
+     memo[n] = solve(n-1,memo)+solve(n-2,memo);
+     return memo[n];
 }
 
-int fib(int n) {
-    vector<int> memo(n + 1, -1);  // initialize memo with -1
-    return fibHelper(n, memo);
-}
+    int fib(int n) {
+        // werre4 using dp first time thanks to radha ji 
+    
+  vector<int>dp (n+1,-1);
+  return solve(n,dp);
+
+        
+    }
 };
