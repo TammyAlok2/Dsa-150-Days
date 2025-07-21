cd9075b2666e8d395717cbe723746c25d7db3b06
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-07-21 10:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (63.57%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        // we use two pointer approach to solve this question 
+        int left = 0;
+        int right = numbers.size()-1;
+
+        while(left < right){
+            int sum = numbers[left]+ numbers[right];
+
+            if(sum == target){
+                return {left+1, right + 1};
+            }
+            else if(sum < target){
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return {-1,-1};
+    }
+};
