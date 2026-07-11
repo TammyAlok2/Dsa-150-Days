907fe575ce7f60b9fd5da66ebe20661c7b37d47c
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2026-07-11 15:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.92%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -5,21 +5,21 @@ public:
         int one = 0;
         int two = nums.size()-1;
 
-        while(one <= two){
-            if(nums[one]==0){
-                swap(nums[zero],nums[one]);
+        while(one <=two){
+            if(nums[one] == 0){
+                swap(nums[one],nums[zero]);
                 zero++;
-                one ++;
-    
-            }
-            else if(nums[one]==1){
                 one++;
             }
-            else{
+            else if(nums[one] ==1){
+                one++;
+            }
+            else if(nums[one] ==2){
                 swap(nums[two],nums[one]);
-           
                 two--;
+                
             }
         }
+        
     }
 };
