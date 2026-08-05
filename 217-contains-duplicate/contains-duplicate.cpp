cd20143cfc48e0bc68f11cbbd2461910f43ee3ea
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2026-08-05 12:06:29 +0530

    Time: 88 ms (13.88%) | Memory: 113.2 MB (14.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
 
         unordered_map<int,int>mp;
 
-        for(int i =0;i<nums.size();i++){
-            mp[nums[i]]++;
+        for(int i =0;i<n;i++){
+             mp[nums[i]]++;
         }
 
-        for (auto it :mp){
-            if(it.second >1){
+        for(auto it:mp){
+            if(it.second > 1){
                 return true;
             }
         }
-        
+
         return false;
     }
 };
