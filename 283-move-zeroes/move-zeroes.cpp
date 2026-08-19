de0fd2ac5aad4ae1a3b6dc573080fb7172437edf
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2026-08-19 10:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (83.17%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // using two pointer approach 
-        // intializing left and right from 0 
-        int left =0;
-        for(int right =0;right<nums.size();right++){
-            if(nums[right]!=0){
-                swap(nums[right],nums[left]);
-                left++;
-            }
-        }
-        
+       int left =0;
+       for(int i =0;i<nums.size();i++){
+         if(nums[i] !=0){
+            swap(nums[left],nums[i]);
+            left ++;
+         }
+       }
     }
 };
