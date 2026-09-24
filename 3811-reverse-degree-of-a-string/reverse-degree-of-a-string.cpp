7207a5106267fb0cc6d8b8b731389311592811ff
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2026-09-24 13:12:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (73.98%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-
-    int calculateReverse (char s,int i){
-        int sum =  (123 - int(s)) * i;
-        return sum ;
-    }
     int reverseDegree(string s) {
-        
-        int sum = 0 ;
-        for (int i =1;i<=s.size();i++){
-            int reverseValue = calculateReverse(s[i-1],i);
-            sum += reverseValue;
+        int sum = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int reverseValue = 'z' - s[i] + 1;
+            sum += reverseValue * (i + 1);
         }
+
         return sum;
     }
 };
